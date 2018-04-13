6098d9ae606d7c7f2acc5d5ea1d339b2337d9f3c
Frederico <fredimpv@gmail.com>
2018-04-12 23:47:36 -0300

    Mudanças

--- firebaseMain.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyCwOrmAK9r6yrRhZRetypNYbpyPiqwa42c",
-    authDomain: "breaking-the-code.firebaseapp.com",
-    databaseURL: "https://breaking-the-code.firebaseio.com/",
-    projectId: "breaking-the-code",
-    storageBucket: "breaking-the-code.appspot.com",
-    messagingSenderId: "124113959400"
-  };
-  firebase.initializeApp(config);
---
+++ js/firebaseMain.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCwOrmAK9r6yrRhZRetypNYbpyPiqwa42c",
+    authDomain: "breaking-the-code.firebaseapp.com",
+    databaseURL: "https://breaking-the-code.firebaseio.com/",
+    projectId: "breaking-the-code",
+    storageBucket: "breaking-the-code.appspot.com",
+    messagingSenderId: "124113959400"
+  };
+  firebase.initializeApp(config);
